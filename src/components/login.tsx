7fcf868bead5710c82de0ffda03a8ec022ae19bd
ohad ben shimol <a@a.com>
2021-11-10 21:12:39 +0200

    ui updates to login page

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -4,54 +4,64 @@ import { doc, getDocs, getFirestore } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import {
   Button,
+  Dimmer,
   Form,
   Grid,
   Header,
+  Icon,
   Image,
+  Loader,
   Message,
   Segment,
 } from "semantic-ui-react";
 import { firebaseConfig } from "./firebase";
-import WordCloud from "react-d3-cloud";
 import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { Link } from "react-router-dom";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+enum PageState {
+  INITIAL,
+  LOADING,
+  ERROR,
+  DONE,
+}
+
 const LoginForm = () => {
-  const [mediaStream, setMediaStream] = useState<MediaStream>();
-  const data = [
-    { text: "Hey", value: 1000 },
-    { text: "lol", value: 200 },
-    { text: "first impression", value: 800 },
-    { text: "very cool", value: 1000000 },
-    { text: "duck", value: 10 },
-  ];
-  const [usersList, setYsersList] = useState(data);
+  const [pageState, setPageState] = useState(PageState.INITIAL);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    const target: any = event.target;
-    const name = target.name.value;
-    const email = target.email.value;
-    const image = target.img.files[0];
-    if (!name || !email || !image) alert("ERROR");
-    const imageType = image.name.split(".")[1];
+    try {
+      setPageState(PageState.LOADING);
+      const target: any = event.target;
 
-    const citiesCol = collection(db, "users");
-    const doc = await addDoc(citiesCol, {
-      name,
-      email,
-      imageType,
-    });
+      const name = target.name.value;
+      const email = target.email.value;
+      const image = target.img.files[0];
 
-    //Add a new document in collection "cities"
-    const storage = getStorage(app, "gs://modular-visitor-331708.appspot.com");
-    const storageRef = ref(storage, `/selfies/${doc.id}.${imageType}`);
+      const imageType = image.name.split(".")[1];
 
-    // 'file' comes from the Blob or File API
-    const uploadResult = await uploadBytes(storageRef, image);
+      const citiesCol = collection(db, "users");
+      const doc = await addDoc(citiesCol, {
+        name,
+        email,
+        imageType,
+      });
 
-    console.log(uploadResult.metadata.md5Hash);
+      const storage = getStorage(
+        app,
+        "gs://modular-visitor-331708.appspot.com"
+      );
+      const storageRef = ref(storage, `/selfies/${doc.id}.${imageType}`);
+
+      const uploadResult = await uploadBytes(storageRef, image);
+
+      console.log(uploadResult.metadata.md5Hash);
+      setPageState(PageState.DONE);
+    } catch {
+      setPageState(PageState.ERROR);
+    }
   };
 
   return (
@@ -63,62 +73,68 @@ const LoginForm = () => {
       >
         <Grid.Column style={{ maxWidth: 450 }}>
           <Header as="h2" color="teal" textAlign="center">
-            ברוכים הבאים לאירוע: האקטון גוגל
+            ברוכים הבאים לאירוע: האקתון גוגל
           </Header>
-          <Form size="large" onSubmit={handleSubmit}>
-            <Segment stacked>
-              <Form.Input
-                fluid
-                iconPosition="left"
-                icon="user"
-                id="name"
-                placeholder="Name"
-              />
-              <Form.Input
-                fluid
-                icon="mail"
-                id="email"
-                iconPosition="left"
-                placeholder="email"
-                type="email"
-              />
+          {(pageState == PageState.LOADING ||
+            pageState == PageState.INITIAL) && (
+            <Form size="large" onSubmit={handleSubmit}>
+              <Segment stacked>
+                {pageState == PageState.LOADING && (
+                  <Dimmer active inverted>
+                    <Loader />
+                  </Dimmer>
+                )}
+                <Form.Input
+                  fluid
+                  iconPosition="left"
+                  icon="user"
+                  id="name"
+                  placeholder="Name"
+                  required
+                />
+                <Form.Input
+                  fluid
+                  icon="mail"
+                  id="email"
+                  iconPosition="left"
+                  placeholder="email"
+                  type="email"
+                  required
+                />
 
-              <div className="ui fluid left icon input">
-                <input placeholder="selfie" type="file" id="img" capture />
-                <i aria-hidden="true" className="camera icon"></i>
-              </div>
+                <div className="ui fluid left icon input">
+                  <input
+                    placeholder="selfie"
+                    type="file"
+                    id="img"
+                    capture
+                    required
+                  />
+                  <Icon name="camera" />
+                </div>
 
-              <hr />
-              <Button color="teal" fluid size="large">
-                הרשמה
-              </Button>
-            </Segment>
-          </Form>
+                <hr />
+                <Button color="teal" fluid size="large">
+                  הרשמה
+                </Button>
+              </Segment>
+            </Form>
+          )}
+          {pageState == PageState.DONE && (
+            <Message>
+              <Message.Header>נרשמת בהצלחה!</Message.Header>
+              התמונות שלך יעברו אליך בסוף האירוע, למעבר להעלאת תמונות לחצו{" "}
+              <Link to="upload">כאן</Link>
+            </Message>
+          )}
+          {pageState == PageState.ERROR && (
+            <Message>
+              <Message.Header>תקלה בהרשמה</Message.Header>
+              אנא נסו להרשם במועד מאוחר יותר
+            </Message>
+          )}
         </Grid.Column>
       </Grid>
-      <WordCloud
-        data={usersList as any}
-        width={500}
-        height={500}
-        font="Times"
-        fontStyle="italic"
-        fontWeight="bold"
-        fontSize={(word) => Math.log2(word.value) * 5}
-        spiral="rectangular"
-        rotate={(word) => word.value % 360}
-        padding={5}
-        random={Math.random}
-        // fill={(d, i) => schemeCategory10ScaleOrdinal(i)}
-        onWordClick={(event, d) => {
-          console.log(`onWordClick: ${d.text}`);
-        }}
-        onWordMouseOver={(event, d) => {
-          console.log(`onWordMouseOver: ${d.text}`);
-        }}
-        onWordMouseOut={(event, d) => {
-          console.log(`onWordMouseOut: ${d.text}`);
-        }}
-      />
     </>
   );
 };
