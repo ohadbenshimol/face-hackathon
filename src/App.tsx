55f075fa7388ed8d49195a659b6e424c8453288e
ohad ben shimol <a@a.com>
2021-11-11 00:07:57 +0200

    logo should work

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,15 @@ import LoginForm from "./components/login";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import UploadForm from "./components/uploadForm";
 import ParticlesComp from "./components/particles";
-import { Menu, Image } from "semantic-ui-react";
+import { Image } from "semantic-ui-react";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <>
       <ParticlesComp />
       <div className="App">
-        <Image
-          src="assets/logo.svg"
-          size="large"
-          wrapped
-          style={{ height: "15vh" }}
-        />
+        <Image src={logo} size="large" wrapped style={{ height: "15vh" }} />
         <Router>
           <Switch>
             <Route exact path="/upload">
