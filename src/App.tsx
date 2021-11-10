5bde7c78f439c850d99922e165f29d37949367b8
Gai Da King <oohiiv1@gmail.com>
2021-11-10 15:40:47 +0000

    routing with upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import LoginForm from "./components/login";
+import {
+    BrowserRouter as Router,
+    Routes, Route, Link
+} from "react-router-dom";
+import UploadForm from "./components/uploadForm";
 
 function App() {
-  return (
-    <div className="App">
-      <LoginForm />
-    </div>
-  );
+
+
+    return (
+        <div className="App">
+            <Router>
+                <Link to="/upload">upload</Link>
+                <Link to="/login">login</Link>
+                <Routes>
+                    <Route path="/">
+                    </Route>
+                    <Route path="/login" element={<LoginForm />}>
+                    </Route>
+                    <Route path="/upload" element={<UploadForm />}>
+                    </Route>
+                </Routes>
+
+            </Router>
+        </div>
+
+    );
 }
 
 export default App;
